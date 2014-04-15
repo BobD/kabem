4af767464b76710d826b1016d5a5591dca6d251b
Bob Donderwinkel <b.donderwinkel@gmail.com>
2014-04-15 13:43:27 +0200

    almost there for a clean working environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       }
     },
 
-    clean: ['build'],
+    clean: ['build', 'src/sass'],
 
     copy: {
       html: {
@@ -101,11 +101,11 @@ module.exports = function(grunt) {
     watch: {
       html: {
         files: ['src/index.html'],
-        tasks: ['copy:html', 'dom_munger:index', 'sass', 'build-bem-pages']
+        tasks: ['copy:html', 'dom_munger:index', 'sass', 'generate-modifier-pages']
       },
       sass: {
         files: ['src/**/**.scss'],
-        tasks: ['import-all-sass', 'sass', 'build-bem-pages']
+        tasks: ['import-all-sass', 'sass', 'generate-modifier-pages']
       },
       css: {
          files: ['src/**/**.css'],
@@ -118,6 +118,53 @@ module.exports = function(grunt) {
 
   });
 
+
+  grunt.registerTask('scaffold-sass', 'Generates folders for all sass files based on BEM classnames in index.html', function() {
+    var _ = require("underscore");
+    var jsdom = require('jsdom').jsdom;
+    var dir = 'src/';
+    var file = dir + 'index.html';
+    var html = grunt.file.read(file);
+    var doc = jsdom(html).parentWindow.document;
+    var bemClassMarker = ''; // BOB::20140415, empty for now. Need to figure out elegant of excluding non BEM class
+    // var bemClasses = doc.querySelectorAll('body[class^="' + bemClassMarker + '"], body *[class^="' + bemClassMarker + '"]');
+    var bemClasses = doc.querySelectorAll('body[class], body *[class]');
+    var classList, BEList = [], beSplit, dirPath, bePath, mPath;
+
+    _.each(bemClasses, function(el){
+      classList = el.getAttribute('class').split(' ');
+
+       _.each(classList, function(c){
+        if(c.indexOf(bemClassMarker) == 0){
+          BEM = splitBEM(c.replace(bemClassMarker, ''), bemClassMarker);
+          BEList.push(BEM.be);
+        }
+       });
+    });
+
+    BEList = _.uniq(BEList);
+
+    _.each(BEList, function(be){
+      beSplit = be.split('__');
+      dirPath = dir + beSplit.join('/');
+      bePath = dirPath + '/_' + be + '.scss';
+      mPath = dirPath + '/_' + be + '_modifiers.scss';
+
+      if(!grunt.file.isDir(dirPath)){
+        grunt.file.mkdir(dirPath);
+      }
+
+      if(!grunt.file.exists(bePath)){
+        grunt.file.write(bePath);
+      }
+      
+      if(!grunt.file.exists(mPath)){
+        grunt.file.write(mPath);
+      }
+      
+    });
+
+  });
 
   // Simplified version of https://www.npmjs.org/package/grunt-sass-directory-import
   grunt.registerTask('import-all-sass', 'Generates a _all.scss file with all sass files imported', function() {
@@ -145,7 +192,7 @@ module.exports = function(grunt) {
   });
 
 
-  grunt.registerTask('build-bem-pages', 'Generate HTML pages for each BEM modifier', function() {
+  grunt.registerTask('generate-modifier-pages', 'Generate HTML pages for each BEM modifier', function() {
     var _ = require("underscore");
     var CSSOM = require('cssom');
     var CSSFile = grunt.file.read('build/css/index.css');
@@ -189,12 +236,23 @@ module.exports = function(grunt) {
     grunt.task.run(tasks);
   });
 
+  function splitBEM(bemClass){
+    var BE = bemClass.split('__');
+    var M = BE.pop().split('_');
+    BE.push(M.shift());
+    BE = BE.join('__');
+    M = M[0];
+
+    return {be: BE, m: M};
+  }
+
 
   // https://www.npmjs.org/package/load-grunt-tasks
   require('load-grunt-tasks')(grunt);
 
+  grunt.registerTask('scaffold', ['scaffold-sass']);
   grunt.registerTask('prepare-css', ['copy:css', 'import-all-sass', 'sass']);
-  grunt.registerTask('prepare-html', ['copy:html', 'build-bem-pages']);
-  grunt.registerTask('default', ['clean', 'prepare-css', 'prepare-html']);
+  grunt.registerTask('prepare-html', ['copy:html', 'generate-modifier-pages']);
+  grunt.registerTask('default', ['clean', 'scaffold-sass', 'prepare-css', 'prepare-html']);
 
 };
