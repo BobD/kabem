889ae2182942098870db971215fbb2bf2d9a10e7
Bob Donderwinkel <b.donderwinkel@gmail.com>
2014-06-09 09:16:38 +0200

    added sub cssRules parsing and better CSS attribute selectors for multi-modifier BEM output

--- tasks/better-bem.js
+++ tasks/better-bem.js
@@ -1,39 +1,48 @@
 module.exports = function(grunt) {
-	 // https://www.npmjs.org/package/rework-npm
-	 grunt.registerTask('better-bem', 'Makes the use of multiple modifiers in a bem classname possible: "block__element_modifier1_modifier_2"', function() {
-	 	var _ = require("underscore");
-	 	var CSSOM = require('cssom');
-    	var CSSFile = grunt.file.read(grunt.option('build-path') + '/source/css/bem.source.css');
-    	var CSSTree = CSSOM.parse(CSSFile);
-    	var classes = [];
-    	var selector;
-    	var allBEM = [];
+    var _ = require("underscore");
+
+     // https://www.npmjs.org/package/rework-npm
+     grunt.registerTask('better-bem', 'Makes the use of multiple modifiers in a bem classname possible: "block__element_modifier1_modifier_2"', function() {
+        var CSSOM = require('cssom');
+        var CSSFile = grunt.file.read(grunt.option('build-path') + '/source/css/bem.source.css');
+        var CSSTree = CSSOM.parse(CSSFile);
+        var classes = [];
+        var selector;
+        var bemListing = bemSelectors(CSSTree.cssRules, []);
         var css = ['@import "bem";'];
+        var bemListingGrouped = _.groupBy(bemListing, function(bem){ return bem.be; });
+        var beListing = _.keys(bemListingGrouped);
 
-    	_.each(CSSTree.cssRules, function(rule){
-    		selector = rule.selectorText;
-    		bem = grunt.splitBEM(selector);
-    		if(_.has(bem, 'be')){
-    			allBEM.push(bem);
-    		}
-    	});
-
-    	var bemGrouped = _.groupBy(allBEM, function(bem){ return bem.be; });
-    	var beList = _.keys(bemGrouped);
-
-    	_.each(beList, function(be){
-    		css.push('*[class^="' + be + '"]{');
-            css.push('@extend .' + be + ';');
-    		_.each(bemGrouped[be], function(be){
-                if(_.has(be, 'm')){
-                    css.push('&[class*="' + be.m + '"]{@extend .' + be.selector + ';}');
+        _.each(beListing, function(be){
+            css.push('*[class="' + be + '"]{@extend .' + be + ' !optional;');
+            // css.push('@extend .' + be + ' !optional;');
+            _.each(bemListingGrouped[be], function(bem){
+                if(_.has(bem, 'm')){
+                    css.push('&*[class*="' + be + '_"][class*="' + bem.m.substring(1) + '"]{@extend .' + bem.selector + ' !optional;}');
                 }
-    		});
-    		css .push('}');
-    	});
+            });
+            css .push('}');
+        });
 
         var cssString = css.join('');
 
-    	grunt.file.write(grunt.option('kabem-path') + '/bem.better.scss', cssString);
-	 });
+        grunt.file.write(grunt.option('kabem-path') + '/bem.better.scss', cssString);
+     });
+
+    function bemSelectors(cssRules, bemListing){
+        _.each(cssRules, function(rule){
+            if(_.has(rule, 'cssRules')){
+                bemSelectors(rule.cssRules, bemListing);
+                return;
+            }
+
+            selector = rule.selectorText;
+            bem = grunt.splitBEM(selector);
+            if(_.has(bem, 'be')){
+                bemListing.push(bem);
+            }
+        });
+
+        return bemListing;
+    }
 }
