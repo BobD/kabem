36ddc2942128193e75bc804711938cb9f3043511
Bob Donderwinkel <b.donderwinkel@gmail.com>
2014-06-09 09:53:21 +0200

    still better better-bem

--- tasks/better-bem.js
+++ tasks/better-bem.js
@@ -14,13 +14,11 @@ module.exports = function(grunt) {
         var beListing = _.keys(bemListingGrouped);
 
         _.each(beListing, function(be){
-            css.push('*[class="' + be + '"]{@extend .' + be + ' !optional;}');
             _.each(bemListingGrouped[be], function(bem){
                 if(_.has(bem, 'm')){
                     css.push('*[class^="' + be + '_"][class*="' + bem.m.substring(1) + '"]{@extend .' + be + ' !optional; @extend .' + bem.selector + ' !optional;}');
                 }
             });
-            // css .push('');
         });
 
         var cssString = css.join('');
